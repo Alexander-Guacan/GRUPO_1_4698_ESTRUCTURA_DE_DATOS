bddb63aa663400d7b293aad58d2faedfc2bdb114
Paola Moncayo <apmoncayo2@espe.edu.ec>
2022-05-23 16:08:34 -0500

    Actualizando main.cpp en RECURSIVIDAD

--- RECURSIVIDAD/main.cpp
+++ RECURSIVIDAD/main.cpp
@@ -1,89 +1,34 @@
 #include <iostream>
-
-char* decimalABinario(char*, int);
-int siguienteNumeroPrimo(int);
-int minimoComunMultiplo(int, int, int, int);
-int maximoComunDivisor(int, int, int, int);
-
-int siguienteNumeroPrimo(int numeroPrimo) {
-    bool esNumeroPrimo = false;
-    numeroPrimo++;
-
-    while (!esNumeroPrimo) {
-        int iterador{ 1 };
-        int divisores{};
-        while (divisores <= 2 && iterador <= numeroPrimo) {
-            if (numeroPrimo % iterador == 0)
-                divisores++;
-
-            iterador++;
-        }
-
-        esNumeroPrimo = divisores == 2;
-
-        if (!esNumeroPrimo)
-            numeroPrimo++;
-    }
-
-    return numeroPrimo;
-}
-
-int minimoComunMultiplo(int valor1, int valor2, int valor3, int numeroPrimo) {
-
-    if (valor1 == 1 && valor2 == 1 && valor3 == 1) {
-        return 1;
-    }
-    else if (valor1 % numeroPrimo == 0 || valor2 % numeroPrimo == 0 || valor3 % numeroPrimo == 0) {
-        if (valor1 % numeroPrimo == 0) valor1 /= numeroPrimo;
-        if (valor2 % numeroPrimo == 0) valor2 /= numeroPrimo;
-        if (valor3 % numeroPrimo == 0) valor3 /= numeroPrimo;
-
-        return numeroPrimo * minimoComunMultiplo(valor1, valor2, valor3, numeroPrimo);
-    } else {
-        return minimoComunMultiplo(valor1, valor2, valor3, siguienteNumeroPrimo(numeroPrimo));
-    }
-}
-
-int maximoComunDivisor(int valor1, int valor2, int valor3, int numeroPrimo) {
-    if (valor1 == 1 && valor2 == 1 && valor3 == 1) {
-        return 1;
-    } else if (valor1 % numeroPrimo == 0 && valor2 % numeroPrimo == 0 && valor3 % numeroPrimo == 0) {
-        valor1 /= numeroPrimo;
-        valor2 /= numeroPrimo;
-        valor3 /= numeroPrimo;
-
-        return numeroPrimo * maximoComunDivisor(valor1, valor2, valor3, numeroPrimo);
-    } else if (valor1 % numeroPrimo == 0 || valor2 % numeroPrimo == 0 || valor3 % numeroPrimo == 0) {
-        if (valor1 % numeroPrimo == 0) valor1 /= numeroPrimo;
-        if (valor2 % numeroPrimo == 0) valor2 /= numeroPrimo;
-        if (valor3 % numeroPrimo == 0) valor3 /= numeroPrimo;
-
-        return maximoComunDivisor(valor1, valor2, valor3, numeroPrimo);
-    } else {
-        return maximoComunDivisor(valor1, valor2, valor3, siguienteNumeroPrimo(numeroPrimo));
-    }
-}
-
-char* decimalABinario(char* destino, int valor) {
-    if (valor >= 1) {
-        destino = decimalABinario(destino, valor / 2);
-
-        *destino = (valor % 2) + '0';
-        destino++;
-    }
-
-    return destino;
-}
+#include "TransformarDecimalBinario.h"
+#include "Matematica.h"
 
 int main() {
-    int valor1 = 96;
-    int valor2 = 160;
-    int valor3 = 24;
 
-    int mcm = minimoComunMultiplo(valor1, valor2, valor3, 2);
-    int mcd = maximoComunDivisor(valor1, valor2, valor3, 2);
+    Matematica matematica;
+    TransformarDecimalBinario DecimalABinario;
 
-    std::cout << "Resultado: " << mcd << '\n';
+    std :: cout << "TRANSFORMAR DE DECIMAL A BINARIO" << std :: endl;
+    std::cout << "Ingrese un numero: " << std :: endl ;
+    int valor = 0;
+    std :: cin >> valor;
+    char * Transformacion = new char [10]{};
+    DecimalABinario.decimalABinario(Transformacion,valor);
+    std :: cout << "Resultado: " << Transformacion << std::endl  <<std :: endl;
+
+    std :: cout <<"MINIMO COMUN MULTIPLO Y MAXIMO COMUN DIVISOR" <<std :: endl;
+    int valor1, valor2, valor3;
+    std :: cout <<"Ingrese valor1 : " <<std ::endl;
+    std :: cin >> valor1;
+    std :: cout <<"Ingrese valor2 : " <<std ::endl;
+    std :: cin >> valor2;
+    std :: cout <<"Ingrese valor3 : " <<std ::endl;
+    std :: cin >> valor3;
+
+    int mcm = matematica.minimoComunMultiplo(valor1,valor2,valor3,2);
+    int mcd = matematica.maximoComunDivisor(valor1,valor2,valor3,2);
+
+    std :: cout << "Resultado Minimo Comun Multiplo: " << mcm << std :: endl << std :: endl;
+    std :: cout << "Resultado Maximo Comun Divisor: " << mcd << std :: endl << std :: endl;
 
     return 0;
 }

05bc6630962b4e7ee32765e4e0a19a4604900bc0
Alexander-Guacan <adguacan@espe.edu.ec>
2022-06-02 09:41:16 -0500

    actualizando encabezado

--- LISTAENLAZADASIMPLE/main.cpp
+++ LISTAENLAZADASIMPLE/main.cpp
@@ -3,7 +3,7 @@
  * Crear un programa que permita hacer operaciones basicas con listas enlazadas simples (insertar, buscar, eliminar, imprimir)
  * Autor: Alexander David Guacán Rivera y Alison Paola Moncayo Achilchisa
  * Fecha de creacion: 26/05/2022
- * Fecha de ultima modificación: 26/05/2022
+ * Fecha de ultima modificación: 02/06/2022
  * Grupo: 1
  * Github: https://github.com/Alexander-Guacan/GRUPO_1_4698_ESTRUCTURA_DE_DATOS.git
  */

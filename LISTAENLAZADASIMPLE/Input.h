78be41eaf11a9aae0db1c8857bb4faf9bb7b83b1
Alexander-Guacan <adguacan@espe.edu.ec>
2022-06-02 09:39:43 -0500

    Agregando metodos de ingreso de numeros reales y letras, y añadiendo parametro para el tamañÃo del array

--- LISTAENLAZADASIMPLE/Input.h
+++ LISTAENLAZADASIMPLE/Input.h
@@ -4,7 +4,9 @@
 class Input {
     
     public:
-        int integerNumber(const char *instructionMessage);
+        int integerNumber(const char *instructionMessage, int charAmount);
+        double realNumber(const char *instructionMessage, int charAmount);
+        char *letters(const char *instructionMessage, int charAmount);
 };
 
 #endif

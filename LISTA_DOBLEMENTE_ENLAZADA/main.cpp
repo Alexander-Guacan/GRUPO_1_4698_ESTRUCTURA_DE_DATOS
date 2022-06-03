1fa8499c76732980ee2e0ae2e017f6ad5b15941a
Alexander-Guacan <adguacan@espe.edu.ec>
2022-06-03 02:26:39 -0500

    agregando encabezado

---
+++ LISTA_DOBLEMENTE_ENLAZADA/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * UNIVERSIDAD DE LAS FUERZAS ARMADAS - ESPE
+ * Generar un programa que permita manejar listas doblemente enlazadas a través de un menu
+ * Autor: Alexander David Guacán Rivera y Alison Paola Moncayo Achilchisa
+ * Fecha de creacion: 02/06/2022
+ * Fecha de ultima modificación: 03/06/2022
+ * Grupo: 1
+ * Github: https://github.com/Alexander-Guacan/GRUPO_1_4698_ESTRUCTURA_DE_DATOS.git
+ */
+
+#include "Menu.h"
+
+int main() {
+    Menu menu;
+
+    menu.start();
+
+    return 0;
+}

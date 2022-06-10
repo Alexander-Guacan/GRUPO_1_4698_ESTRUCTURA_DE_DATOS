1caa225685557b17c31a54c26d97922364b1e557
Alexander-Guacan <adguacan@espe.edu.ec>
2022-06-10 09:14:29 -0500

    agregando encabezado

--- PROYECTO_PRIMER_PARCIAL/main.cpp
+++ PROYECTO_PRIMER_PARCIAL/main.cpp
@@ -1,3 +1,13 @@
+/*
+ * UNIVERSIDAD DE LAS FUERZAS ARMADAS - ESPE
+ * Generar un programa que gestione la compra y venta de celulares en una tienda
+ * Autor: Alexander David Guacán Rivera y Alison Paola Moncayo Achilchisa
+ * Fecha de creacion: 05/06/2022
+ * Fecha de ultima modificación: 10/06/2022
+ * Grupo: 1
+ * Github: https://github.com/Alexander-Guacan/GRUPO_1_4698_ESTRUCTURA_DE_DATOS.git
+ */
+
 #include "LoginManager.hpp"
 
 int main() {

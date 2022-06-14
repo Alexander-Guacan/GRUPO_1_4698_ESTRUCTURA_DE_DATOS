ecc1fca3501e670f5713dcb6ddd88daf11955671
Alexander-Guacan <adguacan@espe.edu.ec>
2022-06-13 20:59:28 -0500

    agregando encabezado

--- PILAS_COLAS/main.cpp
+++ PILAS_COLAS/main.cpp
@@ -1,3 +1,13 @@
+/*
+ * UNIVERSIDAD DE LAS FUERZAS ARMADAS - ESPE
+ * (enunciado del problema)
+ * Autor: Alexander David Guacán Rivera y Alison Paola Moncayo Achilchisa
+ * Fecha de creacion: 13/06/2022
+ * Fecha de ultima modificación: 13/06/2022
+ * Grupo: 1
+ * Github: https://github.com/Alexander-Guacan/GRUPO_1_4698_ESTRUCTURA_DE_DATOS.git
+ */
+
 #include "HandleConsole.hpp"
 
 int main() {

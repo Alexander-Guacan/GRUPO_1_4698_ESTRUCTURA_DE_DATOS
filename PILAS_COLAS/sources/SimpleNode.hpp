faa62d8947a6e6de32f5044ff7ef0d342d76f881
Paola Moncayo <apmoncayo2@espe.edu.ec>
2022-06-14 19:50:48 -0500

    Agregando encabezados en las clases .hpp

--- PILAS_COLAS/sources/SimpleNode.hpp
+++ PILAS_COLAS/sources/SimpleNode.hpp
@@ -1,3 +1,13 @@
+/*
+ * UNIVERSIDAD DE LAS FUERZAS ARMADAS - ESPE
+ * Trabajar con colas y pilas, generar sus operaciones basicas y construir un metodo que permite obtener la varianza en cada tipo de estructura
+ * Autor: Alexander David Guacán Rivera y Alison Paola Moncayo Achilchisa
+ * Fecha de creacion: 13/06/2022
+ * Fecha de ultima modificación: 14/06/2022
+ * Grupo: 1
+ * Github: https://github.com/Alexander-Guacan/GRUPO_1_4698_ESTRUCTURA_DE_DATOS.git
+ */
+
 #ifndef SIMPLE_NODE_HPP
 #define SIMPLE_NODE_HPP
 

f1f22aca6b1962ba6b745db89b6b7f5fe7aeec18
Alexander-Guacan <adguacan@espe.edu.ec>
2022-06-26 20:40:01 -0500

    testing a funcion buscar

--- HASH_ABIERTO/Sources/main.cpp
+++ HASH_ABIERTO/Sources/main.cpp
@@ -1,17 +1,22 @@
 #include "HashOpen\HashInt.hpp"
+#include <iostream>
 
 int main() {
     HashInt hashTable;
 
     hashTable.insert(4);
     hashTable.insert(2);
-    hashTable.insert(9);
-    hashTable.insert(9);
+    hashTable.insert(8);
+    hashTable.insert(37);
     hashTable.insert(9);
     hashTable.insert(9);
     hashTable.insert(9);
 
     hashTable.print();
+
+    std::cout << "\nPosition: " << hashTable.search(66);
+    std::cout << "\nPosition: " << hashTable.search(37);
+    std::cout << "\nPosition: " << hashTable.search(2);
 
     return 0;
 }

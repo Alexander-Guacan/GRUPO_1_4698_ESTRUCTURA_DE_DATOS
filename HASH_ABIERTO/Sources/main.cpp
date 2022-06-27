1824062638843b0c5aa6f59ba79d789528049c03
Alexander-Guacan <adguacan@espe.edu.ec>
2022-06-27 01:17:30 -0500

    Implementando menu principal

--- HASH_ABIERTO/Sources/main.cpp
+++ HASH_ABIERTO/Sources/main.cpp
@@ -1,22 +1,6 @@
-#include "HashOpen\HashInt.hpp"
-#include <iostream>
+#include "Menus\MenuPrincipal.hpp"
 
 int main() {
-    HashInt hashTable;
-
-    hashTable.insert(4);
-    hashTable.insert(2);
-    hashTable.insert(8);
-    hashTable.insert(37);
-    hashTable.insert(9);
-    hashTable.insert(9);
-    hashTable.insert(9);
-
-    hashTable.print();
-
-    hashTable.deleteElement(8);
-
-    std::cout << std::endl;
-    hashTable.print();
+    MenuPrincipal::start();
     return 0;
 }

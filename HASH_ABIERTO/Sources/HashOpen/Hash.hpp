67b1cf703cce1a48ac159f4e6106dff6f4e9833e
Alexander-Guacan <adguacan@espe.edu.ec>
2022-06-26 20:38:03 -0500

    Implementando funciones isEmpty y size

--- HASH_ABIERTO/Sources/HashOpen/Hash.hpp
+++ HASH_ABIERTO/Sources/HashOpen/Hash.hpp
@@ -6,13 +6,22 @@
 template <typename T>
 class Hash {
     protected:
-        static constexpr short MAX_CAPACITY = 29;
+        static constexpr short MAX_CAPACITY{29};
         Node<T> *array[MAX_CAPACITY]{};
+        short elements{};
 
     private:
         virtual short hashCode(const T &element) = 0;
 
     public:
+        short size() {
+            return this->elements;
+        }
+
+        bool isEmpty() {
+            return size() <= 0;
+        }
+
         virtual void insert(const T &element) = 0;
         virtual short search(const T &element) = 0;
         virtual bool deleteElement(const T &element) = 0;

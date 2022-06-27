ad868284eca4ad9214fa88d12cb88fc86e40420b
Alexander-Guacan <adguacan@espe.edu.ec>
2022-06-26 19:34:25 -0500

    testing de funcion insertar e imprimir

--- HASH_ABIERTO/Sources/main.cpp
+++ HASH_ABIERTO/Sources/main.cpp
@@ -1,34 +1,17 @@
-template <typename T>
-class Node {
-    public:
-        T element{};
-        Node *nextNode{nullptr};
-
-        Node(const T &initialElement) {
-            this->element = initialElement;
-        }
-
-};
-
-template <typename T>
-class Hash {
-    public:
-        virtual void insert(const T &element) = 0;
-};
-
-class HashString : public Hash<int> {
-    private:
-        Node<int> *array[10]{};
-    public:
-        void insert(const int &element) {
-            Node<int> *newNode = new Node<int>(element);
-            array[4] = newNode;
-        }
-};
+#include "HashOpen\HashInt.hpp"
 
 int main() {
-    HashString hashTable;
-    hashTable.insert(5);
+    HashInt hashTable;
+
+    hashTable.insert(4);
+    hashTable.insert(2);
+    hashTable.insert(9);
+    hashTable.insert(9);
+    hashTable.insert(9);
+    hashTable.insert(9);
+    hashTable.insert(9);
+
+    hashTable.print();
 
     return 0;
 }

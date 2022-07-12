129157ef90e4fc9e75a95bcb330be74f574bbb0c
Alexander-Guacan <adguacan@espe.edu.ec>
2022-07-12 10:15:51 -0500

    Reorganizando carpeta

--- LIBRERIAS_Y_UTILIDADES/build/CMakeFiles/General_Library.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for General_Library.
-# This may be replaced when dependencies are built.
--- LIBRERIAS_Y_UTILIDADES/sources/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "..\lib\Menus\MenuPrincipal.hpp"
-
-int main() {
-    MenuPrincipal::start();
-    return 0;
-}
